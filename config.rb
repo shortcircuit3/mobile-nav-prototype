4e17af734b8ff15652681263e0a1f8b251193374
Alex Miles <howdy@alexmilesdesign.com>
2013-04-16 20:34:15 +0200

    Add dropdown and icons

--- config.rb
+++ config.rb
@@ -53,7 +53,7 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-set :fonts_dor, 'fonts'
+set :fonts_dir, 'fonts'
 
 # Build-specific configuration
 configure :build do
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,9 @@
 //= require_tree .
+
+(function($) {
+
+  $('body').on('click', '.hamburger', function(){
+    $('.dropdown').toggleClass('visible');
+  });
+
+})(jQuery);
