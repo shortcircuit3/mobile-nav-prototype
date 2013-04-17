c058d8051d42484b70606e349589fa87c4e938af
Alex Miles <howdy@alexmilesdesign.com>
2013-04-17 16:05:03 +0200

    Finished logged in view

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,8 +2,27 @@
 
 (function($) {
 
+  // Reveal the navigation menu
   $('body').on('click', '.hamburger', function(){
     $('.dropdown').toggleClass('visible');
+  });
+
+  // Reveal the search form
+  $('body').on('click', '.search', function(){
+    if ($('.dropdown').hasClass('visible')){
+      $('.dropdown').removeClass('visible');
+    }
+    $('.logo').addClass('hidden');
+    $('.toolbar-icons').addClass('hidden');
+    $('.search-form').removeClass('hidden');
+
+  });
+
+  // Hide the search form
+  $('body').on('click', '.cancel', function(){
+    $('.logo').removeClass('hidden');
+    $('.toolbar-icons').removeClass('hidden');
+    $('.search-form').addClass('hidden');
   });
 
 })(jQuery);
