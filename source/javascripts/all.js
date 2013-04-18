ea66f0ea796d0b39f8a3171b83ef258212f91092
Alex Miles <howdy@alexmilesdesign.com>
2013-04-18 13:25:35 +0200

    basic search animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,23 +3,29 @@
 (function($) {
 
   // Reveal the navigation menu
-  $('body').on('touchend', '.hamburger', function(){
+  $('body').on('click', '.hamburger', function(){
     $('.dropdown').toggleClass('visible');
   });
 
   // Reveal the search form
-  $('body').on('touchend', '.search', function(){
+  $('body').on('click', '.search', function(){
     if ($('.dropdown').hasClass('visible')){
       $('.dropdown').removeClass('visible');
     }
     $('.logo').addClass('hidden');
-    $('.toolbar-icons').addClass('hidden');
+    //$('.toolbar-icons').addClass('hidden');
     $('.search-form').removeClass('hidden');
+    //$('.toolbar-icons').css('float', 'none');
+    $(this).css({
+      'right': '100%',
+      'margin-right' : '-48px',
+      'border-right': '1px solid rgba(255,255,255,0.3)'
+    });
 
   });
 
   // Hide the search form
-  $('body').on('touchend', '.cancel', function(){
+  $('body').on('click', '.cancel', function(){
     $('.logo').removeClass('hidden');
     $('.toolbar-icons').removeClass('hidden');
     $('.search-form').addClass('hidden');
