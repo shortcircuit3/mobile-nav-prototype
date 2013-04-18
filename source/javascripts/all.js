bdd23e361f90ccc03088b87adefae11cecc34119
Alex Miles <howdy@alexmilesdesign.com>
2013-04-18 19:31:31 +0200

    Cleaned up animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,30 +2,55 @@
 
 (function($) {
 
-  // Reveal the navigation menu
-  $('body').on('touchend', '.hamburger', function(){
-    $('.dropdown').toggleClass('visible');
+  var isSearchOpen = false;
+
+  $('body').on('touchend', '.content', function(){
+    if (isSearchOpen === true) {
+      $('.blue-bar2').removeClass('inview');
+    }
   });
 
-  // Reveal the search form
-  $('body').on('touchend ', '.search', function(e){
-    e.preventDefault();
+  // Reveal the navigation menu
+  $('body').on('touchend', '.hamburger', function(){
+    if(isSearchOpen === true){
+      $('.blue-bar2').removeClass('inview');
+      $('.hamburger').removeClass('hidden');
+      isSearchOpen = false;
+      return false;
+    } else {
+      $('.dropdown').toggleClass('visible');
+    }
+
+    // if ($('.blue-bar2').hasClass('inview')) {
+    //   $('.blue-bar2').removeClass('inview');
+    //   $('.hamburger').removeClass('hidden');
+    //   $('.back').removeClass('visible');
+    //   return false;
+    // }
+  });
+
+  function onSearchTouchEnd(e){
+    if (e) { e.preventDefault(); }
+
     if ($('.dropdown').hasClass('visible')){
       $('.dropdown').removeClass('visible');
+      isSearchOpen = true;
       return false;
     }
 
     $('.blue-bar2').addClass('inview');
-    $('.hamburger').addClass('hidden');
+    //$('.hamburger').addClass('hidden');
     $('.back').addClass('visible');
     // $('.search-field').focus();
-  });
+    isSearchOpen = true;
+  }
+
+  // Reveal the search form
+  $('body').on('touchend ', '.search', onSearchTouchEnd);
 
   // Hide the search form
   $('body').on('touchend', '.back', function(){
-    $('.blue-bar2').removeClass('inview');
-    $('.hamburger').removeClass('hidden');
-    $('.back').removeClass('visible');
+
   });
 
 })(jQuery);
